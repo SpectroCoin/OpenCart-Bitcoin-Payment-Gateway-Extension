e544114ec2bfcf1f97e886d9d405597c216f0759
Rokas Reizgys <rokas.student@gmail.com>
2024-02-26 14:37:00 +0200

    migrating to new api

--- admin/controller/payment/spectrocoin.php
+++ admin/controller/payment/spectrocoin.php
@@ -7,9 +7,9 @@ class Spectrocoin extends \Opencart\System\Engine\Controller
     private $langs = array('heading_title', 'text_edit', 'text_enabled', 'text_disabled', 'text_all_zones', 'text_none',
         'text_yes', 'text_no', 'text_off', 'entry_project', 'entry_client_id', 'entry_client_secret', 'entry_sign', 'entry_lang', 'help_lang', 'entry_test',
         'entry_order_status', 'entry_geo_zone', 'entry_receive_currency', 'entry_status', 'entry_default_payments', 'entry_display_payments',
-        'entry_sort_order', 'help_callback', 'button_save', 'button_cancel', 'tab_general', 'text_default_title', 'entry_title',
-        'text_spectrocoin', 'help_private' , 'info_heading', 'info_desc', 'info_step_1', 'info_step_2', 'info_step_3', 
-        'info_step_4', 'info_step_5', 'info_step_6', 'info_step_7', 'info_step_8', 'info_step_9', 'info_note', 'status_checkbox_label', 'help_merchant', 'help_project', 'help_status'
+        'entry_sort_order', 'button_save', 'button_cancel', 'tab_general', 'text_default_title', 'entry_title',
+        'text_spectrocoin' , 'info_heading', 'info_desc', 'info_step_1', 'info_step_2', 'info_step_3', 
+        'info_step_4', 'info_step_5', 'info_step_6', 'info_step_7', 'info_step_8', 'info_step_9', 'info_step_10', 'info_note', 'status_checkbox_label'
     );
 
     public function index() {
@@ -18,9 +18,7 @@ class Spectrocoin extends \Opencart\System\Engine\Controller
         $this->document->setTitle($this->language->get('heading_title'));
         $this->load->model('setting/setting');
         $this->document->addStyle('view/stylesheet/spectrocoin.css');
-        if ($this->request->server['REQUEST_METHOD'] == 'POST') {
-            $privateKey = isset($this->request->post['spectrocoin_private_key']) ? $this->request->post['spectrocoin_private_key'] : $this->config->get('spectrocoin_private_key');
-    
+        if ($this->request->server['REQUEST_METHOD'] == 'POST') {    
             if ($this->validate()) {
                 $this->load->model('setting/setting');
                 $this->request->post['spectrocoin_private_key'] = $privateKey;
@@ -31,7 +29,6 @@ class Spectrocoin extends \Opencart\System\Engine\Controller
             }
         }
         $data = array();
-//        $data['receive_currency'] = $this->receiveCurrency;
         $this->loadLang($data);
 
         if (!empty($this->error)) {
@@ -44,11 +41,10 @@ class Spectrocoin extends \Opencart\System\Engine\Controller
         $data['action'] = $this->url->link('extension/spectrocoin/payment/spectrocoin', 'user_token=' . $this->session->data['user_token'], 'SSL');
         $data['cancel'] = HTTP_SERVER . 'index.php?route=extension/payment&user_token=' . $this->session->data['user_token'];
 
-        $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_project');
-        $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_merchant');
         $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_title');
-        $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_private_key');
-//        $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_receive_currency');
+        $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_project');
+        $this->loadFromRequestOrFromConfig($data, 'payment_client_id');
+        $this->loadFromRequestOrFromConfig($data, 'payment_client_secret');
         $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_status');
         $this->loadFromRequestOrFromConfig($data, 'payment_spectrocoin_sort_order');
         $data['callback'] = HTTP_CATALOG . 'index.php?route=payment/spectrocoin/callback';
--- admin/language/en-gb/payment/spectrocoin.php
+++ admin/language/en-gb/payment/spectrocoin.php
@@ -21,13 +21,6 @@ $_['text_edit']          = 'Edit Spectrocoin';
 $_['entry_status']       = 'Status:';
 $_['entry_sort_order']   = 'Sort Order:';
 
-// Help
-$_['help_callback']      = '';
-$_['help_private'] = 'Private key is not shown. If you want to change it, just enter your new private key to text box.';
-$_['help_merchant'] = 'Enter your merchant ID. You can find it in your SpectroCoin project settings.';
-$_['help_project'] = 'Enter your project ID. You can find it in your SpectroCoin project settings.';
-$_['help_status'] = 'If set to \'On\', the Spectrocoin payment method will be enabled on checkout.';
-
 // Text
 $_['heading_title']      = 'SpectroCoin';
 $_['text_title']         = 'Spectrocoin.com';

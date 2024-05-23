309b7cc1fb2d0ef4df9651e87b1703a57c05b197
Rokas Reizgys <rokas.student@gmail.com>
2024-05-23 11:38:36 +0300

    fixing callback

--- catalog/controller/payment/cancel.php
+++ catalog/controller/payment/cancel.php
@@ -8,23 +8,35 @@ class Cancel extends \Opencart\System\Engine\Controller
     {
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
+
         $this->load->model('checkout/order');
-        $order = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-        if ($order) {
-            $this->model_checkout_order->addOrderHistory($order['order_id'], 7);
+        
+        if (isset($this->session->data['order_id'])) {
+            $order_id = $this->session->data['order_id'];
+            $order = $this->model_checkout_order->getOrder($order_id);
+            if ($order) {
+                $this->model_checkout_order->addOrderHistory($order_id, 7); // 7 - Canceled
+            }
+        } else {
+            $this->log->write('SpectroCoin Cancel: Order ID is not set in the session.');
         }
-        $this->language->load('payment/spectrocoin');
+
+        $this->language->load('extension/spectrocoin/payment/spectrocoin');
+        
         $data = array();
         $data['title'] = sprintf($this->language->get('heading_title'), '/index.php?route=checkout/cart');
+        
         if (isset($this->request->server['HTTPS']) && $this->request->server['HTTPS'] == 'on') {
-            $data['base'] = HTTP_SERVER;
+            $data['base'] = HTTPS_SERVER;
         } else {
             $data['base'] = HTTP_SERVER;
         }
-        $data['continue'] = HTTP_SERVER . '/index.php?route=checkout/cart';
+        
+        $data['continue'] = $this->url->link('checkout/cart');
         $data['heading_title'] = $this->language->get('heading_title');
         $data['text_failure'] = $this->language->get('text_failure');
         $data['text_failure_wait'] = $this->language->get('text_failure_wait');
+        
         $template = 'extension/spectrocoin/payment/spectrocoin_failure';
         $this->response->setOutput($this->load->view($template, $data));
     }
